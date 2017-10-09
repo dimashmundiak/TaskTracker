dd3ce211942526a2a3e7356f79d8cf7203870cb7
Dima Shmundiak <dimashmund@gmail.com>
2017-10-09 11:26:53 +0300

    added taks editing

--- js/common.js
+++ js/common.js
@@ -12,11 +12,12 @@ function show() {
         })
         .then(function (tasks) {
             console.log(tasks);
-            var html = '<thead><tr><th>Name</th><th>Description</th><th>Delete</th></tr></thead><tbody>';
+            var html = '<thead><tr><th>Name</th><th>Description</th><th>Actions</th></tr></thead><tbody>';
             for (var i = 0, len = tasks.length; i < len; i++) {
                 var task = tasks[i];
-                var deleteButton = '<button class=\'btn btn-danger\' onclick="deleteTask(\'' + task._id + '\')">x</button>'
-                html += '<tr>' + '<td>' + tasks[i].name + '</td>' + '<td>' + tasks[i].description + '</td>' + '<td>' + deleteButton + '</td>' + '</tr>';
+                var deleteButton = '<button class=\'btn btn-danger\' onclick="deleteTask(\'' + task._id + '\')">Delete</button>';
+                var editButton = '<button class=\'btn btn-primary\' onClick="showEditWindow(\'' + task._id + '\',\'' + task.name + '\',\'' + task.description + '\')">Edit</button>'
+                html += '<tr>' + '<td>' + tasks[i].name + '</td>' + '<td>' + tasks[i].description + '</td>' + '<td>' + deleteButton + editButton + '</td>' + '</tr>';
             }
             html += '</tbody>'
             document.getElementById('tasks').innerHTML = html;
@@ -36,5 +37,29 @@ function createTask() {
         .then(show());
     return false;
 };
+
+function updateTask() {
+    var taskId = document.getElementById('editTaskId').value;
+    var taskName = document.getElementById('editTaskName').value;
+    var taskDescription = document.getElementById('editTaskDescription').value;
+    fetch('tasks/' + taskId, {
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: taskName, description: taskDescription })
+    })
+        .then(show());
+    return false;
+};
+
+function showEditWindow(id, name, description) {
+    document.getElementById("editWindow").style.display = "block";
+    document.getElementById("addWindow").style.display = "none";
+    document.getElementById("editWindow").scrollIntoView();
+    document.getElementById("editTaskId").value = id;
+    document.getElementById("editTaskName").value = name;
+    document.getElementById("editTaskDescription").value = description;
+}
 
 show();
